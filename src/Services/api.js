a1e3121bf05b9559a489612b3a1bf6dc9ff67a51
Mohamed <bambamohamed2001@gmail.com>
2025-08-25 15:08:35 +0000

    Version 25/08/2025

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://192.168.252.227:8080/api';
+const API_BASE_URL = 'http://192.168.252.222:8080/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
